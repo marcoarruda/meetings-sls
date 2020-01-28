b344c7d0bd8fd16ac7f52e32817113392d194d19
Rodrigo Sousa <rodrigudesousa@gmail.com>
2020-01-28 10:32:41 -0400

    post funcionando

--- functions/reuniao_agendar.js
+++ functions/reuniao_agendar.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports.handler = async event => {
-  console.log(event)
-
+  let {inicio, fim, sala_id} = event;
+  console.log(inicio);
   let reuniao = {
     reuniao_id: 1,
     user_id: '23456789-jhgfd-7890-kjhgfd',
-    sala_id: 1,
-    inicio: new Date(),
-    fim: new Date()
-  }
+    sala_id: sala_id,
+    inicio: inicio,
+    fim: fim
+  };
 
-  return reuniao
+  return reuniao;
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
