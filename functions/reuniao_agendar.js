598109833d2d979665d649a2ed6153b17eabf9cb
Marco Arruda <marco.nc.arruda@gmail.com>
2020-01-28 19:09:50 -0400

    throw error - 50%

--- functions/reuniao_agendar.js
+++ functions/reuniao_agendar.js
@@ -1,24 +1,27 @@
 'use strict'
 
 module.exports.handler = async event => {
-  let {inicio, fim, sala_id} = event
+  try {
+    let { inicio, fim, sala_id } = event
 
-  let reuniao = {
-    reuniao_id: 1,
-    user_id: '23456789-jhgfd-7890-kjhgfd',
-    sala_id: sala_id,
-    inicio: inicio,
-    fim: fim
+    let reuniao = {
+      reuniao_id: 1,
+      sala_id: sala_id,
+      user_id: '23456789-jhgfd-7890-kjhgfd',
+      inicio: inicio,
+      fim: fim
+    }
+
+    if (sala_id == 1) {
+      throw new Error('invalid argument for sala_id')
+    } else {
+      return reuniao
+    }
+  } catch(ex) {
+    const response = {
+      statusCode: 400,
+      erro: ex.message,
+    }
+    throw new Error(JSON.stringify(response))
   }
-
-  const responseError = {
-    statusCode: 400,
-    erro: 'qlqer coisa!'
-  }
-  // throw new Error(JSON.stringify(responseError))
-
-  return reuniao;
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 }
