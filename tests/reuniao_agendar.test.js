2ab9582f238ce32388cbab5341883ccffe095900
Marco Arruda <marco.nc.arruda@gmail.com>
2020-01-28 14:46:18 -0400

    setup unit tests

---
+++ tests/reuniao_agendar.test.js
@@ -0,0 +1,14 @@
+const chai = require("chai")
+const expect = chai.expect
+
+const { Sala } = require("../layer/models/index")
+
+const reuniao_agendar = require("../functions/reuniao_agendar").handler
+
+describe("Teste reuniao_agendar", () => {
+  // Definir variaveis
+
+  // Setup de testes
+  before(async () => {
+  })
+})
