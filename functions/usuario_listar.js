38de0ca2b264472518c5cb9f9ac88ef3b76c9914
Marco Arruda <marco.nc.arruda@gmail.com>
2020-02-19 14:46:24 -0400

    endpoint - ok

--- functions/usuario_listar.js
+++ functions/usuario_listar.js
@@ -7,7 +7,7 @@ function wrapper() {
   const userPool = new AWS.CognitoIdentityServiceProvider({ region: 'us-east-1' })
 
   var params = {
-    AttributesToGet: [],
+    AttributesToGet: ["name", "email"],
     Filter: "",
     Limit: 10,
     UserPoolId: "us-east-1_qToVlCku5"
@@ -28,6 +28,14 @@ module.exports.handler = async event => {
 
   let users = await wrapper()
 
-  return users
+  return users.Users.map(item => ({
+    id: item.Username,
+    name: item.Attributes.find(a => a.Name == "name").Value,
+    email: item.Attributes.find(a => a.Name == "email").Value,
+    created: item.UserCreateDate,
+    modified: item.UserLastModifiedDate,
+    enabled: item.Enabled,
+    status: item.UserStatus,
+  }))
 
 }
